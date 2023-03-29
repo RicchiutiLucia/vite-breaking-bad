b3d85b92c21e8c597d040e64c8e1b3fcae41c9da
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-29 14:25:07 +0200

    creazione componente header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script>
+ import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+    components: {
+      AppHeader,
+    },
+    data() {
+      return {
+        store
+      }
+    },
+    methods: {
+      getCharacters() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician')
+        .then(response => {
+          this.store.charactersList = response.data.results;
+        });
+      }
+    },
+    created() {
+      this.getCharacters();
+    }
+  }
 
 </script>
 
 <template>
+     <AppHeader title="Yu-Gi-Oh Api"></AppHeader>
 </template>
 
-<style>
+<style lang="scss">
+  @use './styles/general.scss';
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+    <div class="d-flex">
+        <a>
+            <img src="../../public/logo.png" width="70" alt="Yu-Gi-Oh">{{title}}
+        </a>
+    </div>
+       
+</header>
+
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/general.scss' as *;
+    .logo{
+        color: $white;
+        padding: 20px;
+        font-size: 30px;
+    }
+    header{
+    padding: 20px;
+    height: 70px;
+        a{
+            font-size: 30px;
+            text-decoration: none;
+            vertical-align: middle;
+        }
+  }
+</style>
